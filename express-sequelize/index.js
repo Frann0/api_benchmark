e1c2a1b7345d1262d5e63ce6fd397a0ab6ef0bc9
Mike Hovedskov <mikehovedskov@gmail.com>
2024-06-10 21:29:32 +0200

    express sequelize

--- express-sequelize/index.js
+++ express-sequelize/index.js
@@ -1,13 +1,35 @@
-const express = require('express');
+const express = require("express");
+const { User, Product } = require("./sequelize/models");
 
 const app = express();
-const port = 4000;
+const port = 3001;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/users", async (req, res) => {
+  const users = await User.findAll();
+  res.status(200).send(users);
+});
+
+app.get("/users/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+  const user = await User.findOne({ where: { id: Number(id) } }).then((user) =>
+    user.toJSON()
+  );
+  res.status(200).send(user);
+});
+
+app.get("/products", async (req, res) => {
+  const products = await Product.findAll();
+  res.status(200).send(products);
+});
+
+app.get("/products/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+  const product = await Product.findOne({ where: { id: Number(id) } }).then(
+    (product) => product.toJSON()
+  );
+  res.status(200).send(product);
 });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
---
+++ express-sequelize/sequelize/models.js
@@ -0,0 +1,126 @@
+const { Sequelize, DataTypes, Model } = require("sequelize");
+const sequelize = new Sequelize('test_api', 'root', 'root1234', {
+  host: 'localhost',
+  dialect: 'mysql' // specify the dialect here
+});
+
+class User extends Model {}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+    },
+    name: {
+      type: DataTypes.STRING,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: Sequelize.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      defaultValue: Sequelize.NOW,
+      onUpdate: Sequelize.NOW,
+    },
+  },
+  {
+    sequelize,
+    modelName: "User",
+    tableName: "User",
+  }
+);
+
+class Product extends Model {}
+
+Product.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+    },
+    price: {
+      type: DataTypes.FLOAT,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: Sequelize.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      defaultValue: Sequelize.NOW,
+      onUpdate: Sequelize.NOW,
+    },
+  },
+  {
+    sequelize,
+    modelName: "Product",
+    tableName: "Product",
+  }
+);
+
+class Order extends Model {}
+
+Order.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+    },
+    productId: {
+      type: DataTypes.INTEGER,
+    },
+    quantity: {
+      type: DataTypes.INTEGER,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: Sequelize.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      defaultValue: Sequelize.NOW,
+      onUpdate: Sequelize.NOW,
+    },
+  },
+  {
+    sequelize,
+    modelName: "Order",
+    tableName: "Order",
+  }
+);
+
+class WriteTest extends Model {}
+
+WriteTest.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    sequelize,
+    modelName: "WriteTest",
+    tableName: "WriteTest",
+  }
+);
+
+module.exports = { User, Product, Order, WriteTest };
